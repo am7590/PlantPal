a1897b1fd1cec971f001ff7a418aa02f7c9ad3e2
Alek Michelson <am7590@rit.edu>
2024-05-20 20:29:42 -0400

    Tests

--- PlantPalTests/HealthDataViewModelTests.swift
+++ PlantPalTests/HealthDataViewModelTests.swift
@@ -5,78 +5,71 @@
 //  Created by Alek Michelson on 5/18/24.
 //
 
-//import XCTest
-//@testable import PlantPal
-//@testable import PlantPalCore
-//@testable import PlantPalSharedUI
+import XCTest
+@testable import PlantPal
+@testable import PlantPalCore
+@testable import PlantPalSharedUI
 
-//class TestHelpers {
-//    static let sharedGrpcViewModel = GRPCViewModel()
-//    
-//    static let healthPrediction = HealthPrediction(probability: 0.34, binary: false, threshold: 2.0)
-//    static let healthResponse = HealthAssessmentResponse(result: HealthResult(isPlant: healthPrediction, isHealthy: healthPrediction, disease: DiseaseSuggestion(suggestions: [])))
-//}
+class TestHelpers {
+    static let healthPrediction = HealthPrediction(probability: 0.34, binary: false, threshold: 2.0)
+    static let healthResponse = HealthAssessmentResponse(result: HealthResult(isPlant: healthPrediction, isHealthy: healthPrediction, disease: DiseaseSuggestion(suggestions: [])))
+}
 
-//class HealthDataViewModelTests: XCTestCase {
-//    var viewModel: HealthDataViewModel!
-//    var mockService: MockHealthDataService!
-//    var sampleImage: UIImage!
-//
-//    override func setUp() {
-//        super.setUp()
-//        mockService = MockHealthDataService()
-//        viewModel = HealthDataViewModel(service: mockService)
-//        sampleImage = UIImage(systemName: "leaf")! // Make sure this image exists in your asset catalog or system images.
-//    }
-//
-//    override func tearDown() {
-//        viewModel = nil
-//        mockService = nil
-//        super.tearDown()
-//    }
-//
-//    func testHealthDataViewModel_LoadingState() {
-//        // Prepare
-//        mockService.mockResponse = .success(TestHelpers.healthResponse)
-//
-//        // Execute
-//        viewModel.fetchData(for: "Test Plant", id: "123", image: sampleImage)
-//
-//        // Assert
-//        XCTAssertEqual(viewModel.loadState, .loading, "ViewModel should be in loading state immediately after fetching starts.")
-//    }
-//
-//    func testHealthDataViewModel_SuccessfulFetch() {
-//        // Prepare
-//        let suggestion = IdentificationSuggestion(id: "1", name: "Test Disease", probability: 0.99, similarImages: [])
-//        mockService.mockResponse = .success(TestHelpers.healthResponse)
-//
-//        let expectation = self.expectation(description: "Successful fetch of health data")
-//        viewModel.fetchData(for: "Test Plant", id: "123", image: sampleImage)
-//
-//        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-//            if self.viewModel.healthData != nil && self.viewModel.loadState == .loaded {
-//                expectation.fulfill()
-//            }
-//        }
-//
-//        waitForExpectations(timeout: 2, handler: nil)
-//        XCTAssertEqual(viewModel.healthData?.result.isHealthy.probability, 0.99)
-//    }
-//
-//    func testHealthDataViewModel_FailureFetch() {
-//        // Prepare
-//        mockService.mockResponse = .failure(NSError(domain: "TestError", code: 1, userInfo: nil))
-//
-//        let expectation = self.expectation(description: "Failed fetch of health data")
-//        viewModel.fetchData(for: "Test Plant", id: "123", image: sampleImage)
-//
-//        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-//            if self.viewModel.loadState == .failed {
-//                expectation.fulfill()
-//            }
-//        }
-//
-//        waitForExpectations(timeout: 2, handler: nil)
-//    }
-//}
+class HealthDataViewModelTests: XCTestCase {
+    var viewModel: HealthDataViewModel!
+    var mockService: MockHealthDataService!
+    var sampleImage: UIImage!
+
+    override func setUp() {
+        super.setUp()
+        mockService = MockHealthDataService()
+        viewModel = HealthDataViewModel(service: mockService)
+        sampleImage = UIImage(systemName: "leaf")!
+    }
+
+    override func tearDown() {
+        viewModel = nil
+        mockService = nil
+        super.tearDown()
+    }
+
+    func testHealthDataViewModel_LoadingState() {
+        mockService.mockResponse = .success(TestHelpers.healthResponse)
+
+        viewModel.fetchData(for: "Test Plant", id: "123", image: sampleImage)
+
+        XCTAssertEqual(viewModel.loadState, .loading, "ViewModel should be in loading state immediately after fetching starts.")
+    }
+
+    func testHealthDataViewModel_SuccessfulFetch() {
+        let suggestion = IdentificationSuggestion(id: "1", name: "Test Disease", probability: 0.99, similarImages: [])
+        mockService.mockResponse = .success(TestHelpers.healthResponse)
+
+        let expectation = self.expectation(description: "Successful fetch of health data")
+        viewModel.fetchData(for: "Test Plant", id: "123", image: sampleImage)
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            if self.viewModel.healthData != nil && self.viewModel.loadState == .loaded {
+                expectation.fulfill()
+            }
+        }
+
+        waitForExpectations(timeout: 2, handler: nil)
+        XCTAssertEqual(viewModel.healthData?.result.isHealthy.probability, 0.99)
+    }
+
+    func testHealthDataViewModel_FailureFetch() {
+        mockService.mockResponse = .failure(NSError(domain: "TestError", code: 1, userInfo: nil))
+
+        let expectation = self.expectation(description: "Failed fetch of health data")
+        viewModel.fetchData(for: "Test Plant", id: "123", image: sampleImage)
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            if self.viewModel.loadState == .failed {
+                expectation.fulfill()
+            }
+        }
+
+        waitForExpectations(timeout: 2, handler: nil)
+    }
+}
--- PlantPalTests/IdentificationViewModelTests.swift
+++ PlantPalTests/IdentificationViewModelTests.swift
@@ -5,75 +5,73 @@
 //  Created by Alek Michelson on 5/18/24.
 //
 
-//import XCTest
-//@testable import PlantPalCore
-//@testable import PlantPalSharedUI
-//@testable import PlantPal
+import XCTest
+@testable import PlantPalCore
+@testable import PlantPalSharedUI
+@testable import PlantPal
 
+class IdentificationViewModelTests: XCTestCase {
+    var viewModel: IdentificationViewModel!
+    var mockService: MockPlantIdentificationService!
+    var grpcViewModel: GRPCViewModel!
+    var plantFormViewModel: SuccuelentFormViewModel!
+    
+    override func setUp() {
+        super.setUp()
+        grpcViewModel = GRPCViewModel()
+        plantFormViewModel = SuccuelentFormViewModel([])
+        mockService = MockPlantIdentificationService()
+        viewModel = IdentificationViewModel(grpcViewModel: grpcViewModel,
+                                            plantFormViewModel: plantFormViewModel,
+                                            identificationService: mockService)
+    }
 
+    override func tearDown() {
+        viewModel = nil
+        mockService = nil
+        grpcViewModel = nil
+        plantFormViewModel = nil
+        super.tearDown()
+    }
 
-//class IdentificationViewModelTests: XCTestCase {
-//    var viewModel: IdentificationViewModel!
-//    var mockService: MockPlantIdentificationService!
-//    var grpcViewModel: GRPCViewModel!
-//    var plantFormViewModel: SuccuelentFormViewModel!
-//    
-//    override func setUp() {
-//        super.setUp()
-//        grpcViewModel = TestHelpers.sharedGrpcViewModel // only one grpc model instance for tests
-//        plantFormViewModel = SuccuelentFormViewModel([])
-//        mockService = MockPlantIdentificationService()
-//        viewModel = IdentificationViewModel(grpcViewModel: grpcViewModel,
-//                                            plantFormViewModel: plantFormViewModel,
-//                                            identificationService: mockService)
-//    }
-//
-//    override func tearDown() {
-//        viewModel = nil
-//        mockService = nil
-//        grpcViewModel = nil
-//        plantFormViewModel = nil
-//        super.tearDown()
-//    }
-//
-//    func testIdentificationViewModel_SuccessfulDataLoad() {
-//        let expectation = self.expectation(description: "Successful data fetch")
-//        
-//        viewModel.fetchData()
-//        
-//        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
-//            if case .loaded = self.viewModel.loadState, self.viewModel.identificationData != nil {
-//                expectation.fulfill()
-//            }
-//        }
-//        
-//        waitForExpectations(timeout: 2.0) { error in
-//            if let error  {
-//                XCTFail("\(#function) Failed with error: \(error)")
-//            }
-//        }
-//    }
-//
-//    func testIdentificationViewModel_HandlesEmptySuggestions() {
-//        mockService.identifyPlantHandler = { _, _, completion in
-//            let response = IdentificationResponse(result: IdentificationResult(classification: IdentificationClassification(suggestions: [])))
-//            completion(.success(response))
-//        }
-//        
-//        let expectation = self.expectation(description: "Empty suggestions gandled")
-//        
-//        viewModel.fetchData()
-//        
-//        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
-//            if case .loaded = self.viewModel.loadState, self.viewModel.identificationData?.result?.classification?.suggestions?.isEmpty == true {
-//                expectation.fulfill()
-//            }
-//        }
-//        
-//        waitForExpectations(timeout: 2.0) { error in
-//            if let error {
-//                XCTFail("\(#function) Failed with error: \(error)")
-//            }
-//        }
-//    }
-//}
+    func testIdentificationViewModel_SuccessfulDataLoad() {
+        let expectation = self.expectation(description: "Successful data fetch")
+        
+        viewModel.fetchData()
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            if case .loaded = self.viewModel.loadState, self.viewModel.identificationData != nil {
+                expectation.fulfill()
+            }
+        }
+        
+        waitForExpectations(timeout: 5.0) { error in
+            if let error  {
+                XCTFail("\(#function) Failed with error: \(error)")
+            }
+        }
+    }
+
+    func testIdentificationViewModel_HandlesEmptySuggestions() {
+        mockService.identifyPlantHandler = { _, _, completion in
+            let response = IdentificationResponse(result: IdentificationResult(classification: IdentificationClassification(suggestions: [])))
+            completion(.success(response))
+        }
+        
+        let expectation = self.expectation(description: "Empty suggestions gandled")
+        
+        viewModel.fetchData()
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            if case .loaded = self.viewModel.loadState, self.viewModel.identificationData?.result?.classification?.suggestions?.isEmpty == true {
+                expectation.fulfill()
+            }
+        }
+        
+        waitForExpectations(timeout: 5.0) { error in
+            if let error {
+                XCTFail("\(#function) Failed with error: \(error)")
+            }
+        }
+    }
+}
