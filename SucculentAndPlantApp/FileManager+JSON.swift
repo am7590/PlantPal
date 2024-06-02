bcb7b2d6fee1b6ebcbf292733ee0b7bcd16dd9f6
Alek Michelson <am7590@rit.edu>
2024-06-02 19:01:08 -0400

    These files were missing for some reason

--- SucculentAndPlantApp/FileManager+JSON.swift
+++ SucculentAndPlantApp/FileManager+JSON.swift
@@ -2,7 +2,46 @@
 //  FileManager+JSON.swift
 //  SucculentAndPlantApp
 //
-//  Created by Alek Michelson on 5/27/24.
+//  Created by Alek Michelson on 5/7/23.
 //
 
-import Foundation
+import UIKit
+import os
+
+extension FileManager {
+
+    func saveJSON(_ json: String, fileName: String) {
+        let url = URL.documentsDirectory.appending(path: fileName)
+        do {
+            try json.write(to: url, atomically: false, encoding: .utf8)
+        } catch {
+            Logger.plantPal.error("Could not save json to url: \(url) with error \(error)")
+        }
+    }
+    
+    func decodeJSON(from url: URL) -> CodableImage? {
+        do {
+            let data = try Data(contentsOf: url)
+            do {
+                return try JSONDecoder().decode(CodableImage.self, from: data)
+            } catch {
+                Logger.plantPal.error("\(#function) \(error.localizedDescription)")
+                return nil
+            }
+        } catch {
+            Logger.plantPal.error("\(#function) \(error.localizedDescription)")
+            return nil
+        }
+    }
+    
+    func moveFile(oldURL: URL, newURL: URL) {
+        if fileExists(atPath: newURL.path) {
+            try? removeItem(at: newURL)
+        }
+        do {
+            try moveItem(at: oldURL, to: newURL)
+        } catch {
+            Logger.plantPal.error("\(#function) \(error.localizedDescription)")
+        }
+    }
+}
